6e10b95b073a163e6ec351a39bd6884734c58472
tonghuix <tonghuix@gmail.com>
2014-10-28 20:42:07 +0000

    update ubuntu installation script

--- install.latex.ubuntu.sh
+++ install.latex.ubuntu.sh
@@ -7,6 +7,6 @@ sudo apt-get install ttf-wqy-microhei ttf-wqy-zenhei # from WenQuanYi
 sudo apt-get install texlive-fonts-recommended
 sudo apt-get install latex-cjk-all
 sudo apt-get install fonts-arphic-gkai00mp fonts-arphic-ukai latex-cjk-chinese-arphic-gkai00mp #install Kaiti font
-sudo apt-get install fonts-ebgaramond fonts-ebgaramond-extra texlive-fonts-extra #Install EBGaramond12
+sudo apt-get install fonts-ebgaramond fonts-ebgaramond-extra fonts-cantarell texlive-fonts-extra #Install EBGaramond12
 sudo apt-get install fonts-arphic-gbsn00lp latex-cjk-chinese-arphic-gbsn00lp 
 sudo fc-cache
